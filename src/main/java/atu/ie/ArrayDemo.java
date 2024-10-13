908cd629d108d4c7539bcd688821e1aa839c929b
matthew <g00430349@atu.ie>
2024-10-13 20:25:42 +0100

    coded up to Promted to add Array Week 5 Array's

---
+++ src/main/java/atu/ie/ArrayDemo.java
@@ -0,0 +1,30 @@
+package atu.ie;
+
+import java.nio.channels.ScatteringByteChannel;
+import java.util.Scanner;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] myArray = new int[10];
+        myArray[0] = 100;
+        myArray[1] = 200;
+        myArray[2] = 300;
+        myArray[3] = 400;
+        myArray[4] = 500;
+        myArray[5] = 600;
+        myArray[6] = 700;
+        myArray[7] = 800;
+        myArray[8] = 900;
+        myArray[9] = 1000;
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter new value of the array:");
+        int newvalue = scanner.nextInt();
+
+        myArray[10] = newvalue;
+        System.out.println("" + myArray[10]);
+    }
+}
